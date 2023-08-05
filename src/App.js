da9a89abc24a42de10a61e62a5ab292f467b1fca
NamanSamant <samant.naman15@gmail.com>
2023-08-05 20:52:44 +0530

    change in route

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ import master from "./components/Restaurant/DifferentSections/master";
 function App() {
   return (
     <>
-      <HomeLayoutHOC path="/Zomato-clone-frontend" exact component={HomeComponent} />
-      <HomeLayoutHOC path="/Zomato-clone-frontend/:type" exact component={Master} />
+      {/* <HomeLayoutHOC path="/Zomato-clone-frontend" exact component={HomeComponent} /> */}
+      <HomeLayoutHOC path="/Zomato-clone-frontend/delivery" exact component={Master} />
       <HomeLayoutHOC path="/Zomato-clone-frontend/resid/:type" exact component={master}/>
       <Routes>
         <Route path="/Zomato-clone-frontend/xy/login" element = <LgSnLayout/> />
