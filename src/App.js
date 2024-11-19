91e9ecad5ed4c5404cc027913a9526b889d7e6a0
NamanSamant <121803108+NamanSamant@users.noreply.github.com>
2024-11-19 15:07:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import HomeLayoutHOC from "./HOC/Home.HOC";
-import Master from "./components/navbar/master";
-import HomeComponent from "./components/Homepage/Home.component";
-import LgSnLayout from "./Layout/LogInPage";
-import SignUpPage from "./Layout/SignUpPage";
+import HomeLayoutHOC from "../HOC/Home.HOC";
+import Master from "../components/navbar/master";
+import HomeComponent from "../components/Homepage/Home.component";
+import LgSnLayout from "../Layout/LogInPage";
+import SignUpPage from "../Layout/SignUpPage";
 import { Route, Routes } from "react-router-dom";
-import temp from "./components/temp";
-import ResLayout from "./Layout/RestaurantLayout";
-import master from "./components/Restaurant/DifferentSections/master";
+import temp from "../components/temp";
+import ResLayout from "../Layout/RestaurantLayout";
+import master from "../components/Restaurant/DifferentSections/master";
 
 
 function App() {
